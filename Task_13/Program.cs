ec00b1e6886a5103422b202f0e5cbb6d3bf150ac
oksanaPC2 <77opod@gmail.com>
2022-07-15 16:47:45 +0300

    задачи 10,13,15

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if(number<100)
+{
+  Console.WriteLine("третьей цифры нет");
+}
+else{
+    string number1=number.ToString();
+    Console.WriteLine(number1[2]);
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1-7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<0 && number>7){
+Console.WriteLine("Ошибка ввода ");
+}
+else{
+    if(number>5){
+        Console.WriteLine("выходной");
+    }
+    else{
+        Console.WriteLine("будний");
+    }
+}
